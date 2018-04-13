b18458dbd68a8287a02a87d07279db646ffd9662
kabytech <kabytech20@gmail.com>
2018-04-13 22:57:30 +0300

    second commit

--- app/src/main/java/com/example/kamere/recyclerview/FruitAdapter.java
+++ app/src/main/java/com/example/kamere/recyclerview/FruitAdapter.java
@@ -2,28 +2,36 @@ package com.example.kamere.recyclerview;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by Kamere on 4/12/2018.
  */
 
-public class FruitAdapter extends RecyclerView.Adapter<FruitAdapter.FruitViewHolder> {
+public class FruitAdapter extends RecyclerView.Adapter<FruitAdapter.FruitViewHolder> implements Filterable{
 
     Context context;
     List<Fruits> fruitsList;
+    List<Fruits> filteredFruitList;
+    private FruitAdapterListener listener;
 
-    public FruitAdapter(Context context, List<Fruits> fruitsList) {
+    public FruitAdapter(Context context, List<Fruits> fruitsList, FruitAdapterListener listener) {
         this.context = context;
+        this.listener = listener;
         this.fruitsList = fruitsList;
-    }
+        this.filteredFruitList = fruitsList;
 
+    }
 
     @Override
     public FruitViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
@@ -36,7 +44,7 @@ public class FruitAdapter extends RecyclerView.Adapter<FruitAdapter.FruitViewHol
     @Override
     public void onBindViewHolder(FruitViewHolder holder, int position) {
 
-        Fruits fruits = fruitsList.get(position);
+        Fruits fruits = filteredFruitList.get(position);
         holder.fName.setText(fruits.getName());
         holder.fDesc.setText(fruits.getDesc());
         holder.fImage.setImageDrawable(context.getResources().getDrawable(fruits.getImage()));
@@ -44,7 +52,48 @@ public class FruitAdapter extends RecyclerView.Adapter<FruitAdapter.FruitViewHol
 
     @Override
     public int getItemCount() {
-        return fruitsList.size();
+        return filteredFruitList.size();
+    }
+
+    @Override
+    public Filter getFilter() {
+
+        return new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence charSequence) {
+                String charString = charSequence.toString();
+                if (charString.isEmpty()) {
+                    filteredFruitList = fruitsList;
+
+                } else {
+                    List<Fruits> filteredList = new ArrayList<>();
+                    for (Fruits row : fruitsList) {
+
+
+                        //filter according to your case
+                        if (row.getName().toLowerCase().contains(charString.toLowerCase()) || row.getDesc().toLowerCase().contains(charString.toLowerCase())) {
+                            filteredList.add(row);
+                        }
+                        Log.i("Car Plate",charString);
+                    }
+
+                    filteredFruitList = filteredList;
+                }
+
+                FilterResults filterResults = new FilterResults();
+                filterResults.values = filteredFruitList;
+
+                return filterResults;
+            }
+
+            @Override
+            protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+                filteredFruitList = (ArrayList<Fruits>) filterResults.values;
+                notifyDataSetChanged();
+
+            }
+        };
+
     }
 
     public class FruitViewHolder extends RecyclerView.ViewHolder {
@@ -57,7 +106,16 @@ public class FruitAdapter extends RecyclerView.Adapter<FruitAdapter.FruitViewHol
             fName = itemView.findViewById(R.id.fname);
             fDesc = itemView.findViewById(R.id.fdesc);
             fImage = itemView.findViewById(R.id.fImage);
-
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+            listener.onFruitSelected(filteredFruitList.get(getAdapterPosition()));
+                }
+            });
         }
+    }
+
+    public interface FruitAdapterListener {
+        void onFruitSelected(Fruits fruits);
     }
 }
--- app/src/main/java/com/example/kamere/recyclerview/MainActivity.java
+++ app/src/main/java/com/example/kamere/recyclerview/MainActivity.java
@@ -1,20 +1,30 @@
 package com.example.kamere.recyclerview;
 
+import android.app.SearchManager;
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.support.v7.widget.SearchView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements FruitAdapter.FruitAdapterListener {
 
     RecyclerView recyclerView;
     List<Fruits> fruitsList;
+    FruitAdapter adapter;
+    SearchView searchView;
 
     //Array of fruits names
     String[] names={"Apple","Strawberry","Pomegranates","Oranges","Watermelon","Bananas","Kiwi","Tomato","Grapes"};
@@ -40,12 +50,25 @@ public class MainActivity extends AppCompatActivity {
         //setting layout
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // toolbar fancy stuff
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("RecyclerView");
+
+        // white background notification bar
+        whiteNotificationBar(recyclerView);
+
         //initialize fruits list
         fruitsList = new ArrayList<>();
 
+
+
+        adapter = new FruitAdapter(this,fruitsList,this);
+
         //method to load fruits
         loadfruits();
-
         //onItemClickListener
         recyclerView.addOnItemTouchListener(new FruitTouchListener(getApplicationContext(), new FruitTouchListener.OnItemClickListener() {
             @Override
@@ -64,9 +87,75 @@ public class MainActivity extends AppCompatActivity {
 
             fruitsList.add(new Fruits(names[i], sntfNames[i], image[i]));
         }
-
-        FruitAdapter adapter = new FruitAdapter(this,fruitsList);
+         adapter.notifyDataSetChanged();
         recyclerView.setAdapter(adapter);
 
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        // Associate searchable configuration with the SearchView
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        searchView = (SearchView) menu.findItem(R.id.action_search)
+                .getActionView();
+        searchView.setSearchableInfo(searchManager
+                .getSearchableInfo(getComponentName()));
+        searchView.setMaxWidth(Integer.MAX_VALUE);
+
+        // listening to search query text change
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                // filter recycler view when query submitted
+                adapter.getFilter().filter(query);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String query) {
+                // filter recycler view when text is changed
+                adapter.getFilter().filter(query);
+                return false;
+            }
+        });
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_search) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        // close search view on back button pressed
+        if (!searchView.isIconified()) {
+            searchView.setIconified(true);
+            return;
+        }
+        super.onBackPressed();
+    }
+
+    private void whiteNotificationBar(View view) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            view.setSystemUiVisibility(0);
+        }
+    }
+
+    @Override
+    public void onFruitSelected(Fruits fruits) {
+        Toast.makeText(getApplicationContext(), "Selected: " + fruits.getName() , Toast.LENGTH_LONG).show();
+    }
+
 }
